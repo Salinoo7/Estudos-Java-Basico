5b181a5d242a9934f151b017c3351c75c9d3e758
salinoo7 <lucassalino3008@outlook.com>
2023-01-09 20:56:09 -0300

    Projeto de Calculadora

---
+++ Exercicios-Metodos/src/Calculadora.java
@@ -0,0 +1,24 @@
+public class Calculadora {
+      
+    public static void soma(double numero1, double numero2) {
+        double resultado = numero1 + numero2;
+        System.out.print(numero1 + "+" + numero2 + "=" + resultado);
+   }
+
+   public static void subritacao(double numero1, double numero2) {
+        double resultado = numero1 - numero2; 
+        System.out.print(numero1 + "+" + numero2 + "=" + resultado);
+   }
+
+   public static void multiplicacao(double numero1, double numero2) {
+        double resultado = numero1 * numero2; 
+        System.out.print(numero1 + "+" + numero2 + "=" + resultado);
+   }
+
+   public static void divisao(double numero1, double numero2) {
+
+        double resultado = numero1 / numero2;
+        System.out.print(numero1 + "+" + numero2 + "=" + resultado);
+   }
+
+}
---
+++ Exercicios-Metodos/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculadora.soma(5, 10);
+        Calculadora.subritacao(15, 5.2);
+        Calculadora.multiplicacao(3, 15);
+        Calculadora.divisao(50, 3);
+    }
+}
